0f2644fd8e58cc8c357503a7c59290d48350e410
Calix <calixmonnet@gmail.com>
2022-03-21 17:43:46 +0000

    card:partial

--- src/components/CardProject.js
+++ src/components/CardProject.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const CardProject = () => {
   return (
-    <div className=''>
-        
+    <div className=' rounded my-3 p-7 border-2'>
+        <h2>Title</h2>
+        <a href='gogle.com' className='text-blue-500  hover:underline my-12 text-xs'>google.com</a>
+        <div className='rounded-full w-full h-4 bg-black my-2'></div>
     </div>
   )
 }
